a5e8fc6776e20c294aff42104d03360d2e727661
sagacemente <41288130+sagacemente@users.noreply.github.com>
2023-05-04 21:47:16 +0200

    Update crypto_dataset.py

--- crypto_dataset.py
+++ crypto_dataset.py
@@ -38,7 +38,7 @@ class Dataset:
 
     def organize_ds_folders(self):
         #############   LABELS TRUTH  
-        train_truth_file_path = os.getcwd() + '/' + self.NAME + self.language  + '/' + self.subtask + '/train_truth.json'
+        train_truth_file_path = os.getcwd() + '/' + self.NAME + '/' + self.subtask + '/train_truth.json'
         #train_truth_file_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/' + self.subtask + '/train_truth.json'
         f = open(train_truth_file_path, "r")
         self.id_label_dict = {}
@@ -50,7 +50,7 @@ class Dataset:
         print('id_label_dict',self.id_label_dict)
 
         #############  TEXTS 
-        train_texts_path = os.getcwd() + '/' + self.NAME + self.language  + '/' + self.subtask + '/train_text.json'
+        train_texts_path = os.getcwd() + '/' + self.NAME  + '/' + self.subtask + '/train_text.json'
         #train_texts_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/'  + self.subtask  + '/train_text.json'
         f = open(train_texts_path, "r")
         self.id_texts_dict = {}
