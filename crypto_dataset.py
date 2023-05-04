d6224be50ba2130b4322ba8062974205d55a2bd9
sagacemente <41288130+sagacemente@users.noreply.github.com>
2023-05-04 21:26:41 +0200

    Update crypto_dataset.py

--- crypto_dataset.py
+++ crypto_dataset.py
@@ -26,8 +26,8 @@ class Dataset:
         
     def fetch_ds_files(self):
         if 'augmented' in self.url:
-            #NAME = 'pan23-profiling-cryptocurrency-influencers-augmented-' + self.language 
-            train_set_archive = tf.keras.utils.get_file('pan23-profiling-cryptocurrency-influencers-augmented-de.zip',self.url,
+            NAME = 'pan23-profiling-cryptocurrency-influencers-augmented-' + self.language + '.zip'
+            train_set_archive = tf.keras.utils.get_file(NAME,self.url,
                                             extract=True, archive_format='zip',cache_dir='.',
                                             cache_subdir='')      
         else:
@@ -37,7 +37,8 @@ class Dataset:
 
     def organize_ds_folders(self):
         #############   LABELS TRUTH  
-        train_truth_file_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/' + self.subtask + '/train_truth.json'
+        train_truth_file_path = os.getcwd() + '/' + NAME + self.language  + '/' + self.subtask + '/train_truth.json'
+        #train_truth_file_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/' + self.subtask + '/train_truth.json'
         f = open(train_truth_file_path, "r")
         self.id_label_dict = {}
         for line in f:
@@ -48,7 +49,8 @@ class Dataset:
         print('id_label_dict',self.id_label_dict)
 
         #############  TEXTS 
-        train_texts_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/'  + self.subtask  + '/train_text.json'
+        train_texts_path = os.getcwd() + '/' + NAME + self.language  + '/' + self.subtask + '/train_text.json'
+        #train_texts_path = os.getcwd() + '/pan23-profiling-cryptocurrency-influencers/'  + self.subtask  + '/train_text.json'
         f = open(train_texts_path, "r")
         self.id_texts_dict = {}
         self.tweet_ids_dict = {}
